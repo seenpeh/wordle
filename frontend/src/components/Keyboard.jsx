fac61d9f7bc5a0e36cedbbdd4dcce1f0735d5cf1
seenpeh <seenpeh@gmail.com>
2024-07-11 23:26:51 +0330

    fixed the keyboard part 1

--- frontend/src/components/Keyboard.jsx
+++ frontend/src/components/Keyboard.jsx
@@ -2,16 +2,32 @@
 import React from 'react';
 import Key from './Key';
 
+
+
 const Keyboard = ({ onKeyPress }) => {
-  const keys = 'QWERTYUIOPASDFGHJKLZXCVBNM'.split('');
+  const firstRow = 'QWERTYUIOP'.split('');
+  const secondRow = 'ASDFGHJKL'.split('');
+  const thirdRow = 'ZXCVBNM'.split('');
 
   return (
     <div className="keyboard">
-      {keys.map((key) => (
-        <Key key={key} letter={key} onClick={onKeyPress} />
-      ))}
-      <Key key="ENTER" letter="ENTER" onClick={onKeyPress} />
-      <Key key="DELETE" letter="DELETE" onClick={onKeyPress} />
+      <div className="row">
+        {firstRow.map((key) => (
+          <Key key={key} letter={key} onClick={onKeyPress} />
+        ))}
+      </div>
+      <div className="row">
+        {secondRow.map((key) => (
+          <Key key={key} letter={key} onClick={onKeyPress} />
+        ))}
+      </div>
+      <div className="row">
+        {thirdRow.map((key) => (
+          <Key key={key} letter={key} onClick={onKeyPress} />
+        ))}
+        <Key key="ENTER" letter="ENTER" onClick={onKeyPress} />
+        <Key key="DELETE" letter="DELETE" onClick={onKeyPress} />
+      </div>
     </div>
   );
 };
